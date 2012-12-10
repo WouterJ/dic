4fbe347897e3e0970113b5a16517ec58c9cb80bf
WouterJ <waldio.webdesign@gmail.com>
2012-12-10 22:16:50 +0100

    Changed PHPdoc comments

--- src/Wj/Dic/InstanceManager/InstanceManagerInterface.php
+++ src/Wj/Dic/InstanceManager/InstanceManagerInterface.php
@@ -3,22 +3,22 @@
 namespace Wj\Dic\InstanceManager;
 
 /**
- * This interface must be implemented by all Instance Managers
+ * This interface must be implemented by all Instance Managers.
  *
  * @author Wouter J <wouter@wouterj.nl>
  */
 interface InstanceManagerInterface
 {
     /**
-     * Register parameters for a instance
+     * Register arguments for a instance.
      *
-     * @param string $name       The name of the instance
-     * @param array  $parameters All parameters of the instance
+     * @param string $name      The name of the instance
+     * @param array  $arguments All arguments of the instance
      */
-    public function registerInstanceParameters($name, array $parameters);
+    public function registerInstanceArguments($name, array $arguments);
 
     /**
-     * Gets a new instance
+     * Gets a new instance.
      *
      * @param string $name The name of the instance
      *
