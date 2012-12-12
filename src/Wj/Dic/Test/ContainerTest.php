cf7fc6c7e67de7257a25f08facea7690affe6b37
WouterJ <waldio.webdesign@gmail.com>
2012-12-12 16:44:05 +0100

    Moved Factory tests to its own class

--- src/Wj/Dic/Test/ContainerTest.php
+++ src/Wj/Dic/Test/ContainerTest.php
@@ -30,47 +30,6 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
 
 
     /*----------------------*\
-        FACTORIES
-    \*----------------------*/
-    public function testFactories()
-    {
-        $c = $this->container;
-
-        $c->setParameter('mailer.transport', 'sendmail');
-        $c->setFactory('mailer', function ($c) {
-            return new \Mailer($c->getParameter('mailer.transport'));
-        });
-
-        $this->assertTrue($c->hasFactory('mailer'));
-
-        $mailer = $c->getFactory('mailer');
-        $this->assertInstanceOf('\Mailer', $mailer);
-        $this->assertEquals('sendmail', $mailer->getTransport());
-    }
-
-    /**
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage The factory ("mailer") must be a callable
-     */
-    public function testThrowExceptionIfFactoryIsNotCallable()
-    {
-        $this->container->setFactory('mailer', 'foo');
-    }
-
-    /**
-     * @expectedException Wj\Dic\Exception\NotFoundException
-     * @expectedExceptionMessage The factory "foo" does not exists
-     */
-    public function testThrowExceptionIfFactoryDoesNotExists()
-    {
-        $this->container->getFactory('foo');
-    }
-
-
-
-
-
-    /*----------------------*\
         INSTANCES
     \*----------------------*/
     public function testInstanceManagerWithStaticParameters()
