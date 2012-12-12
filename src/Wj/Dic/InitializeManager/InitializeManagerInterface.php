653c589a269058bbe4c5651c87e902be6fe9a5c7
WouterJ <waldio.webdesign@gmail.com>
2012-12-12 17:37:35 +0100

    Created InitializeManagerInterface

---
+++ src/Wj/Dic/InitializeManager/InitializeManagerInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Wj\Dic\InitializeManager;
+
+
+use Wj\Dic\Container;
+
+
+/**
+ * This interface must be implemented by all Initialize Managers.
+ *
+ * @author Wouter J <wouter@wouterj.nl>
+ */
+interface InitializeManagerInterface
+{
+    /**
+     * Sets an initializer for a specific interface.
+     *
+     * @param string   $interface The name of the interface
+     * @param callable $factory   A PHP callable to modify the instance
+     */
+    public function setInitializer($interface, $factory);
+
+    /**
+     * Modify an instance with a initializer.
+     *
+     * @param object $instance  The instance to modify
+     * @param string $interface The name of the interface
+     */
+    public function getInstance($instance, $interface);
+
+    /**
+     * @param Container $container
+     */
+    public function setContainer(Container $container);
+}
