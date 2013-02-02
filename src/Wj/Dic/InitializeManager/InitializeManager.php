611f62b3f46e330ae55caafebf37d9a4e4373a69
WouterJ <waldio.webdesign@gmail.com>
2013-02-02 12:30:02 +0100

    Reverted first try to save the build

--- src/Wj/Dic/InitializeManager/InitializeManager.php
+++ src/Wj/Dic/InitializeManager/InitializeManager.php
@@ -4,7 +4,6 @@ namespace Wj\Dic\InitializeManager;
 
 
 use Wj\Dic\Container;
-use Wj\Dic\InitializeManager\InitializeManagerInterface;
 
 
 class InitializeManager implements InitializeManagerInterface
